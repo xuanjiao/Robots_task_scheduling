890c00b08d52d56840d16c2c6f580748056754f2
xuanjiao <Xuanjiao.zhu@stud.uni-due.de>
2020-12-14 21:17:55 +0100

    delete unused files

--- sql/experiment/analysis.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS exp_db.ese_rs_analysis_1;
-CREATE TABLE exp_db.ese_rs_analysis_1
-SELECT exp_id,wt_btr,total, completed , completed/total, TIME_TO_SEC(duration)/(total-expired) FROM exp_db.exe_rs_09_18 WHERE wt_wait = 1  AND wt_psb = -1 AND wt_pri = -1;
-SELECT * FROM exp_db.ese_rs_analysis_1;
-
--- DROP TABLE IF EXISTS exp_db.exe_rs_09_18;
--- CREATE TABLE exp_db.exe_rs_09_18
--- SELECT * FROM origin_db.exe_rs;
--- sql/experiment/robot_number.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO origin_db.exe_rs(wt_btr,wt_wait,wt_psb,wt_pri) VALUES (10,1,-1,-10),(10,1,-1,-10),(10,1,-1,-10);
-
-SELECT * FROM origin_db.exe_rs;
